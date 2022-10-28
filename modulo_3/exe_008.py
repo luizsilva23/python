cb64b2cf2af947e91708643fcc9078dd8981e86e
Luiz Silva <luizgpereiras22@gmail.com>
2022-10-28 01:07:11 -0300

    testes com tuplas

---
+++ modulo_3/exe_008.py
@@ -0,0 +1,11 @@
+lanche = ('hambúrguer', 'suco', 'pizza', 'pudim', 'refrigerante',  'brigadeiro')
+#for comida in lanche:
+#   print(f'Eu vou comer {comida}.')
+
+#for c in range(0, len(lanche)):
+#    print(lanche[c])
+
+#for pos, comida in enumerate(lanche):
+#    print(f'Eu vou comer {comida} na posição {pos}')
+
+print(sorted(lanche))
